b781b5076ca81c7c9cad1464530de4f6a91071be
Narendra Arief Nugraha <narendra.arief.nugraha@gmail.com>
2025-01-06 19:56:53 +0700

    disable form & add debug quality

--- Assets/_Game Assets/Scripts/_Manager/_Global/GlobalGameManager.cs
+++ Assets/_Game Assets/Scripts/_Manager/_Global/GlobalGameManager.cs
@@ -39,36 +39,41 @@ public class GlobalGameManager : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Alpha1))
         {
             soundManager.musicSource.mute = !soundManager.musicSource.mute;
+            Debug.Log("Mute!");
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha2))
         {
             QualitySettings.SetQualityLevel(0, true);
             Screen.SetResolution(960, 540, FullScreenMode.FullScreenWindow, 30);
+            Debug.Log("Quality level 0");
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha3))
         {
             QualitySettings.SetQualityLevel(1, true);
             Screen.SetResolution(1280, 720, FullScreenMode.FullScreenWindow, 60);
+            Debug.Log("Quality level 1");
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha4))
         {
             QualitySettings.SetQualityLevel(2, true);
             Screen.SetResolution(1920, 1080, FullScreenMode.FullScreenWindow, 60);
+            Debug.Log("Quality level 2");
         }
 
         if (Input.GetKeyDown(KeyCode.Alpha5))
         {
             QualitySettings.SetQualityLevel(3, true);
             Screen.SetResolution(3840, 2160, FullScreenMode.FullScreenWindow, 60);
+            Debug.Log("Quality level 3");
         }
     }
 
     private void OnApplicationQuit()
     {
-        Application.OpenURL("https://forms.gle/TaZ6Vkf4QSFH9WS2A");
+        //Application.OpenURL("https://forms.gle/TaZ6Vkf4QSFH9WS2A");
     }
 
     private void HandleSpecs()
