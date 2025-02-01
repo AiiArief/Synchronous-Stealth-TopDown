32f91c8f291a640489a5898f54f7a01ce8afcccc
Narendra Arief Nugraha <narendra.arief.nugraha@gmail.com>
2025-02-01 23:04:21 +0700

    feat: reflection probe tools

---
+++ Assets/_Photo Mode/Scripts/ReflectionProbeTools.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+using System;
+
+public class RefllectionProbeTools
+{
+#if UNITY_EDITOR
+    [MenuItem("Tools/Reflection Probe/Remove All Reflection Probe Optimizer")]
+    static void Remove()
+    {
+        var rpos = GameObject.FindObjectsOfType<ReflectionProbeOptimizer>();
+        Debug.Log($"Found {rpos.Length} reflection probe optimizers, will remove them.");
+
+        try
+        {
+            foreach (var rpo in rpos)
+            {
+                GameObject.DestroyImmediate(rpo);
+            }
+
+            AssetDatabase.Refresh();
+            //while (rpos.Length > 0)
+            //{
+            //    GameObject.Destroy(rpos[0]);
+            //}
+        }
+        catch (Exception ex)
+        {
+            Debug.Log(ex.ToString());
+        }
+    }
+#endif
+}
