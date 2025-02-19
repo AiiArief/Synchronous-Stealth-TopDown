2d11d51399a2361f3931374d8a0f569e6c1ee2b7
Narendra Arief Nugraha <narendra.arief.nugraha@gmail.com>
2025-02-20 01:32:10 +0700

    feat: disable pp if potato

---
+++ Assets/_Game Assets/Scripts/Stored Actions/_Behaviour/PostProcessingOptimizer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+[RequireComponent(typeof(PostProcessVolume))]
+public class PostProcessingOptimizer : MonoBehaviour
+{
+    PostProcessVolume m_postProcessVolume;
+
+    private void Awake()
+    {
+        m_postProcessVolume = GetComponent<PostProcessVolume>();
+
+        OnQualityLevelChanged(QualitySettings.GetQualityLevel());
+    }
+
+    private void OnEnable()
+    {
+        SystemUIManager.OnQualityLevelChanged += OnQualityLevelChanged;
+    }
+
+    private void OnDisable()
+    {
+        SystemUIManager.OnQualityLevelChanged -= OnQualityLevelChanged;
+    }
+
+    /// <summary>
+    /// 0 = potato
+    /// 1 = recommended
+    /// 2 = brute force
+    /// </summary>
+    /// <param name="qualityLevel"></param>
+    private void OnQualityLevelChanged(int qualityLevel)
+    {
+        m_postProcessVolume.enabled = qualityLevel > 0;
+    }
+}
